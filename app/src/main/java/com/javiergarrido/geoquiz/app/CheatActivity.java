c5816f7f2560765eed089d021c728d6aacb64dbe
Javier Garrido <javierationmex@gmail.com>
2014-06-23 01:19:02 -0700

    Initial commit

---
+++ app/src/main/java/com/javiergarrido/geoquiz/app/CheatActivity.java
@@ -0,0 +1,55 @@
+package com.javiergarrido.geoquiz.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by Javier G on 6/14/2014.
+ */
+public class CheatActivity extends ActionBarActivity {
+
+    public static final String EXTRA_ANSWER_IS_TRUE = "com.javiergarrido.geoquiz.answer_is_true";
+    public static final String EXTRA_ANSWER_IS_SHOWN = "com.javiergarrido.geoquiz.answer_is_shown";
+
+    private boolean mAnswerIsTrue;
+
+    private TextView mAnswerTextView;
+    private Button mShowAnswer;
+
+    private void setAnswerShownResult(boolean isAnswerShown) {
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_IS_SHOWN, isAnswerShown);
+        setResult(RESULT_OK, data);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+
+        //Answer will not be shown until the user
+        // presses the button
+        setAnswerShownResult(false);
+
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+
+        mAnswerTextView = (TextView)findViewById(R.id.answerTextView);
+
+        mShowAnswer = (Button)findViewById(R.id.showAnswerButton);
+        mShowAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mAnswerIsTrue) {
+                    mAnswerTextView.setText(R.string.true_button);
+                } else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+                setAnswerShownResult(true);
+            }
+        });
+    }
+}
